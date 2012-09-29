93bf673474bc6f198d0e723be3085f6cc5c6090d
Jason O'Neil <jason.oneil@gmail.com>
2012-09-29 22:23:00 +0800

    Fix for GithubCodeBlocks - now correctly escapes code inside block
    It needs to run before the BlockGamut, which means it's priority needs
    to be < 10.

--- src/filters/GithubCodeBlocks.hx
+++ src/filters/GithubCodeBlocks.hx
@@ -14,15 +14,18 @@ class GithubCodeBlocks implements MarkdownExtension
 {
 	public var inst:Markdown;
 	public var type = MarkdownExtensionType.BLOCK;
-	public var priority = 49;
+	public var priority = 9; // It needs to run before the block gamut
 
 	public function filter(text:String):String
 	{
-
 		// attacklab: sentinel workarounds for lack of \A and \Z, safari\khtml bug
 		text += "~0";
 		
 		text = ~/\n```(.*)\n([^`]+)\n```/g.customReplace(text, function (re: EReg) {
+			if (text.indexOf("In here this gh *code should* not become Markdown") > -1)
+			{
+				//js.Lib.debug();
+			}
 			var language = re.matched(1);
 			var codeblock = re.matched(2);
 		
